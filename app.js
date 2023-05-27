b19fc2a9dd7345dbaebac1a7ad3087b042f23369
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-05-27 16:26:13 -0400

    write and headers

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const http = require('http');
 
 
 http.createServer((req,res) => {
-    console.log(req);
-    res.write('Hola mundo');
+    /*console.log(req); */
+    res.setHeader('Content-Disposition','attachment; filename=lista.csv')
+    res.writeHead(200,{'Content-Type':'application/csv'});
+    res.write('id, nombre\n');
+    res.write('2, Fernando\n');
+    res.write('3, Maria\n');
+    res.write('4, Juan\n');
+    res.write('5, Pedro\n');
     res.end();
 })
 .listen(3000);
